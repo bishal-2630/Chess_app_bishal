6d9b6dd74e225154ef6fac21311315691b20c4c0
Bishal <kbishal177@example.com>
2026-01-11 10:57:18 +0545

    Chess Authorization in swagger

--- chess_backend/settings.py
+++ chess_backend/settings.py
@@ -153,3 +153,15 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ],
 }
+
+SWAGGER_SETTINGS = {
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+            'description': 'JWT Authorization header using the Bearer scheme. Example: "Bearer {token}"'
+        }
+    },
+    'USE_SESSION_AUTH': False,
+}
