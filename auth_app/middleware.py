d4fae1f82052ebacb7e809aabac90406c8bf8e27
Bishal <kbishal177@gmail.com>
2026-01-21 21:32:09 +0545

    Updated asgi and midddleware

--- auth_app/middleware.py
+++ auth_app/middleware.py
@@ -1,5 +1,12 @@
 from django.utils.deprecation import MiddlewareMixin
 from django.conf import settings
+from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AnonymousUser
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
+from urllib.parse import parse_qs
+
 
 class DisableCSRFOnSpecificAPIsMiddleware(MiddlewareMixin):
     def process_request(self, request):
@@ -35,3 +42,36 @@ class DisableCSRFOnSpecificAPIsMiddleware(MiddlewareMixin):
         except Exception as e:
             print(f"❌ HEADER ERROR: {str(e)}")
         return response
+
+User = get_user_model()
+@database_sync_to_async
+def get_user(token_key):
+    try:
+        access_token = AccessToken(token_key)
+        user_id = access_token['user_id']
+        return User.objects.get(id=user_id)
+    except (InvalidToken, TokenError, User.DoesNotExist):
+        return AnonymousUser()
+class JWTAuthMiddleware:
+    def __init__(self, inner):
+        self.inner = inner
+    async def __call__(self, scope, receive, send):
+        token = None
+        headers = dict(scope['headers'])
+        if b'authorization' in headers:
+            try:
+                auth_header = headers[b'authorization'].decode()
+                if auth_header.startswith('Bearer '):
+                    token = auth_header.split(' ')[1]
+            except:
+                pass
+        if not token:
+            query_string = scope.get('query_string', b'').decode()
+            query_params = parse_qs(query_string)
+            if 'token' in query_params:
+                token = query_params['token'][0]
+        if token:
+            scope['user'] = await get_user(token)
+        else:
+            scope['user'] = AnonymousUser()
+        return await self.inner(scope, receive, send)
--- chess_backend/asgi.py
+++ chess_backend/asgi.py
@@ -2,18 +2,17 @@ import os
 import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chess_backend.settings')
 django.setup()
 
-
+# Import from existing middleware file
+from auth_app.middleware import JWTAuthMiddleware
 import chess_backend.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
+    "websocket": JWTAuthMiddleware(
         URLRouter(
             chess_backend.routing.websocket_urlpatterns
         )
